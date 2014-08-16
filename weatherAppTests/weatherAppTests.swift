d3d46a3129dd7e3b0abbdeb716f9389421829f68
leonlin14 <leonlin14@gmail.com>
2014-08-16 13:23:23 +0800

    Initial Commit

---
+++ weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by Lin wei-chih on 2014/8/16.
+//  Copyright (c) 2014年 Lin wei-chih. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class weatherAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
