82cc6e527d8c16b4b7086f59fefe7303d94da2d1
leonlin14 <leonlin14@gmail.com>
2014-08-17 14:01:18 +0800

    check weather status

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController, NSURLConnectionDelegate {
     
     //IBOutlet let storyBoard view Control can see the string
     @IBOutlet var city: UILabel!
+    //x:-16 y:80
     @IBOutlet var icon: UIImageView!
     @IBOutlet var temperature: UILabel!
+    @IBOutlet var humidity: UILabel!
+    @IBOutlet var windSpeed: UILabel!
     
     // 使用 NSMutableData 儲存下載資料
     var data: NSMutableData = NSMutableData()
@@ -30,12 +33,14 @@ class ViewController: UIViewController, NSURLConnectionDelegate {
         self.view.backgroundColor = UIColor(patternImage: background)
         
         //touch the screen for download
-        let singleFingerTap = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
-        self.view.addGestureRecognizer(singleFingerTap)
+        //let singleFingerTap = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        //self.view.addGestureRecognizer(singleFingerTap)
         
-        func handleSingleTap (recognizer: UITapGestureRecognizer) {
-            startConnection()
-        }
+        //func handleSingleTap (recognizer: UITapGestureRecognizer) {
+        //    startConnection()
+        //}
+        
+        startConnection()
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,8 +62,8 @@ class ViewController: UIViewController, NSURLConnectionDelegate {
     func connection(connection: NSURLConnection!, didReceiveData dataReceived: NSData!) {
         println("downloading")
         
-        //self.data.appendData(dataReceived)
-        self.data.setData(dataReceived)
+        self.data.appendData(dataReceived)
+        //self.data.setData(dataReceived)
 
     }
     
@@ -76,13 +81,61 @@ class ViewController: UIViewController, NSURLConnectionDelegate {
         
         // 讀取各項天氣資訊
         let temp: AnyObject? = jsonDictionary["main"]?["temp"]
+        let humidity: AnyObject? = jsonDictionary["main"]?["humidity"]
+        let speed: AnyObject? = jsonDictionary["wind"]?["speed"]
         
-        // 資料處理
+        // use '?' to downcast to NSArray
+        if let weather = jsonDictionary["weather"]? as? NSArray {
+            // Safe code 寫作觀念:
+            // 使用 as? 轉型時，要把以下這行放進 if statement 裡處理
+            let weatherDictionary = (weather[0] as NSDictionary)
+            // 天氣狀態 (多雲、晴朗等等)
+            updateWeatherConditionIcon(weatherDictionary["id"] as Int)
+        }
+        
+        //  溫度資料處理
         let weatherTempCelsius = Int(round((temp!.floatValue - 273.15)))
         let weatherTempFahrenheit = Int(round(((temp!.floatValue - 273.15) * 1.8) + 32))
-            
+        let weatherHumidity = round(humidity!.floatValue)
+        let weatheWindrSpeed = round(speed!.floatValue)
+        
+        // 測試輸出
+        println("temp: \(weatherTempCelsius)℃")
+        println("humidity: \(weatherHumidity)")
+        println("speed: \(weatheWindrSpeed)m/s")
+        
         // 輸出到 UI
+        self.temperature.font = UIFont.boldSystemFontOfSize(28)
         self.temperature.text = "\(weatherTempCelsius)℃"
+        self.humidity.text = "\(weatherHumidity)"
+        self.windSpeed.text = "\(weatheWindrSpeed) m/s"
+
+    }
+    
+    // 解讀 Weather Condition Code
+    // See: http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
+    func updateWeatherConditionIcon(weatherId: Int) {
+        println("weather ID: \(weatherId)")
+        
+        switch weatherId {
+        case 500, 501, 502, 503, 504:
+            self.icon.image = UIImage(named: "RAIN")
+            let background = UIImage(named: "rain_bg320px.png")
+            self.view.backgroundColor = UIColor(patternImage: background)
+            
+        case 800, 801, 802:
+            self.icon.image = UIImage(named: "sun.png")
+            let background = UIImage(named: "sun_bg320px.png")
+            self.view.backgroundColor = UIColor(patternImage: background)
+            
+        case 803, 804:
+            self.icon.image = UIImage(named: "CloudyDay.png")
+            let background = UIImage(named: "CloudyDay_bg320px.png")
+            self.view.backgroundColor = UIColor(patternImage: background)
+            
+        default:
+            println("no weather icon")
+        }
     }
 }
 
